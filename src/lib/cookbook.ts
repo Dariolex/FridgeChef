5be542196f48ee72b5da29566162b02ce28a5530
Dariolex <dariocav@hotmail.it>
2026-09-11 21:16:17 +0200

    Ripristina e completa cookbook: 40 ricette (10 paste, 10 carne, 5 dessert)

--- src/lib/cookbook.ts
+++ src/lib/cookbook.ts
@@ -1,1 +1,12 @@
-PLACEHOLDER
+import type { Diet, Ingredient, Prefs, Recipe } from "./types";
+
+export const FOOD_ART = {
+  avocado: "/graphics/avocado.jpg",
+  tomato: "/graphics/tomato.jpg",
+  lettuce: "/graphics/lettuce.jpg",
+  onion: "/graphics/onion.jpg",
+  lemon: "/graphics/lemon.jpg",
+  pepper: "/graphics/pepper.jpg",
+  mushrooms: "/graphics/mushrooms.jpg",
+  hero: "/graphics/hero.jpg",
+} as const;
